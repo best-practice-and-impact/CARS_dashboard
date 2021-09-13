5b5006451c4947515a3a48103eb96ba43b26acd2
Dan Shiloh <daniel.shiloh@ons.gov.uk>
2021-09-13 11:34:53 +0100

    bug fix: now counting responses correctly

--- main.R
+++ main.R
@@ -2,11 +2,9 @@ source("R/ingest.R")
 source("R/data_cleaning.R")
 source("R/plot.R")
 
-departments <- remove_low_counts(ingest_department_data())
-
 rmarkdown::render("rmarkdown/index.rmd", output_file = "../docs/index.html")
 
 gert::git_branch_checkout("main")
-gert::git_add("main.R")
-gert::git_commit("automating commit and push", author = "Jacob <jacob.cole@ons.gov.uk>")
+gert::git_add("docs/")
+gert::git_commit("Latest data", author = "Jacob <jacob.cole@ons.gov.uk>")
 gert::git_push()
