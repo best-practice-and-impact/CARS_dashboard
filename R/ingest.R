4e76f81bbfaebfdfbb9d85889e190ffe52746751
Cole <Jacob.Cole@ons.gov.uk>
2023-10-12 14:23:40 +0100

    Fix broken dashboard

--- R/ingest.R
+++ R/ingest.R
@@ -1,38 +1,79 @@
-#'@title Ingest department data
+#' @title Get tidy data from API
 #'
-#'@description get department data from the smartcurvey API. 
+#' @description API function for data ingest. Loads data from the smartsurvey API and convert to a tidy data frame.
 #'
-#'@return department column from CARS wave 3 data (character).
+#' @param ... optional: arguments to be passed to \link{ingest}
 #'
+#' @return tidied CARS dataset
+#'
+#' @export
 
-ingest_department_data <- function() {
+get_tidy_data_api <- function() {
   
-  export_id <- get_export_id(get_export_list())  
-  data <- convert_raw(ingest(export = export_id))
+  ingest()
   
-  # Discard top 2 rows as these are blank
-  departments <- data$Q3..Which.department.do.you.primarily.work.in.[3:length(data$Q3..Which.department.do.you.primarily.work.in.)]
+  data <- ingest() %>%
+    convert_raw()
   
-  return(departments)
+  return(data)
 }
 
-#'@title Ingest smartsurvey data
+#' @title Convert raw data to data frame
 #'
-#'@description Download smartsurvey export via the API. Download the exported data from smartsurvey using the API. Use convert_raw() to convert the API response to a data.frame.
+#' @description Convert raw smartsurvey data to data.frame . Extract contents (raw csv) from smartsurvey API request and convert to data.frame
 #'
-#'@param survey the survey ID (character string/numeric). Defaults to "790800".
-#'@param export the export ID (character string/numeric).
-#'@param token the API token (character string). Loaded from environment variable by default.
-#'@param secret the secret API token (character string). Loaded from environment variable by default.
+#' @param r api response object
 #'
-#'@return the exported data as a dataframe
+#' @return response content as a data.frame
+
+convert_raw <- function(r) {
+  
+  if (class(r) != "response") {
+    stop("Unexpected input - r is not a response object.")
+  } else if (r$status_code != 200) {
+    stop("Unsuccessful API request - no data.")
+  }
+  
+  content <- rawToChar(r$content)
+  
+  data <- utils::read.table(
+    text = content,
+    sep = ",",
+    header = TRUE,
+    fill = TRUE,
+    quote = c("\"\"", "'"),
+    na.strings = c("", ".", "NA", "-", "\"\"", "\".\"", "\"NA\"", "\"-\"")
+  )
+  
+  # Fix apostrophes
+  data[] <- lapply(data, function(x) gsub("@SQ@", "'", x))
+  
+  return(data)
+}
+
+
+#' @title Ingest smartsurvey data
 #'
-#'@export
+#' @description Download smartsurvey export via the API. Download the exported data from smartsurvey using the API. Use convert_raw() to convert the API response to a data.frame.
+#' Note: the first API request in a session will typically fail.
+#'
+#' @param survey the survey ID (character string/numeric). Defaults to "1167489".
+#' @param token the API token (character string). Loaded from environment variable by default.
+#' @param secret the secret API token (character string). Loaded from environment variable by default.
+#' @param proxies proxy addresses (string). Loads from the user environment by default. Expects a string in the format "ip.address:port; alt.ip.address:port".
+#' @param export the export ID (character string/numeric).
+
+#' @return the exported data as a dataframe
 
 ingest <- function(survey = "1167489",
-                   export,
                    token = Sys.getenv("CARS_TOKEN"),
-                   secret = Sys.getenv("CARS_SECRET")) {
+                   secret = Sys.getenv("CARS_SECRET"),
+                   proxies = Sys.getenv("alt_proxy"),
+                   export) {
+  
+  if (missing(export)) {
+    export <- "latest"
+  }
   
   # Check input types
   if (!is.character(survey) && !is.numeric(survey) | !is.character(export) && !is.numeric(export)) {
@@ -50,115 +91,31 @@ ingest <- function(survey = "1167489",
   # API request
   url <- paste0("https://api.smartsurvey.io/v1/surveys/", survey, "/exports/", export, "/download")
   
-  auth <- httr::authenticate(user = token, password = secret, type = "basic")
-  
-  tryCatch(
-    {
-      r <- httr::GET(
-        url, 
-        auth
-      )    
-    },
-    error = function(e) {
-      stop(paste("Error in API request: ", e))
+  if (!is.null(proxies)) {
+    proxy_list <- stringr::str_split(proxies, ";")[[1]]
+    
+    for (proxy_address in proxy_list) {
+      httr::set_config(httr::use_proxy(proxy_address))
+      
+      tryCatch(
+        {
+          r <- httr::GET(
+            url,
+            httr::authenticate(user = token, password = secret, type = "basic")
+          )
+        },
+        error = function(e) {
+          warning(paste("Error in API request, trying alternative proxy: ", e))
+        }
+      )
+      
+      if (exists("r") && r$status_code == 200) {
+        break
+      }
+      
     }
-  )
-  
-  # Check request status code
-  if (r$status_code != 200) {
-    stop(paste0("Unsuccessful API request. Status code: ", r$status_code))
-    return(r)
+    
   }
   
   return(r)
-}
-
-#'@title Convert raw data to data.frame
-#'
-#'@description Convert raw smartsurvey data to data.frame . Extract contents (raw csv) from smartsurvey API request and convert to data.frame
-#'
-#'@param r api response object
-#'
-#'@return response content as a data.frame
-#'
-#'@export
-
-convert_raw <- function(r) {
-  
-  if (class(r) != "response") {
-    stop("Unexpected input - r is not a response object.")
-  } else if (r$status_code != 200) {
-    stop("Unsuccessful API request - no data.")
-  }
-  
-  content <- rawToChar(r$content)
-  
-  data <- utils::read.table(
-    text = content, 
-    sep = ",", 
-    header = TRUE, 
-    fill = TRUE, 
-    quote = "\"\"",
-    na.strings = c("", ".", "NA", "-", "\"\"", "\".\"", "\"NA\"", "\"-\"")
-  )
-  
-  return(data)
-}
-
-#'@title get list of exports
-#'
-#'@description retrieve a list of data exports of the CARS wave 3 data
-#'
-#'@param survey survey number (string)
-#'@param token API token. This should be stored as an environment variable and NOT kept in this repository. The function will retrieve the token from the environment variable.
-#'@param secret API secret. This should be stored as an environment variable and NOT kept in this repository. The function will retrieve the token from the environment variable.
-#'
-#'@return API request
-#'
-
-get_export_list <- function(survey = "1167489", token = Sys.getenv("CARS_TOKEN"), secret = Sys.getenv("CARS_SECRET")) {
-  
-  # API request
-  url <- paste0("https://api.smartsurvey.io/v1/surveys/", survey, "/exports/") 
-  
-  auth <- httr::authenticate(user = token, password = secret, type = "basic")
-  
-  
-  tryCatch(
-    {
-      r <- httr::GET(
-        url, 
-        auth
-      )    
-    },
-    error = function(e) {
-      stop(paste("Error in API request: ", e))
-    }
-  )
-  
-  return(r)
-}
-
-#'@title get CSV export ID
-#'
-#'@description Retrieve the latest export's ID from the export list
-#'
-#'@param r API request object returned by get_export_list
-#'@param export_n desired export index (1 = latest export, 2 = previous export, etc.). Defaults to 1.
-#'
-#'@return export ID (string)
-#'
-
-get_export_id <- function(r, export_n) {
-  
-  if (r$status_code != 200) {
-    stop(paste0("Unsuccessful API request - status code: ", r$status_code, "."))
-  }
-  
-  export_list <- XML::xmlParse(rawToChar(r$content))
-  export_list <- XML::xmlToList(export_list)
-  latest_dataset_id <- export_list[export_n]$exports$Id
-  
-  return(latest_dataset_id)
-  
 }
