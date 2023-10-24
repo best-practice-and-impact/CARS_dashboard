9dc21dcd356a53d06f8d62012aaa24a786d3f8b3
shilohd <daniel.shiloh@ons.gov.uk>
2023-10-24 17:37:45 +0100

    Update

--- main.R
+++ main.R
@@ -3,8 +3,3 @@ source("R/data_cleaning.R")
 source("R/plot.R")
 
 rmarkdown::render("rmarkdown/index.rmd", output_file = "../docs/index.html")
-
-gert::git_branch_checkout("main")
-gert::git_add("docs/")
-gert::git_commit("Latest data", author = "Jacob <jacob.cole@ons.gov.uk>")
-gert::git_push()
